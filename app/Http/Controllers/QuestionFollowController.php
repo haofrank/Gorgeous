be0a1765121324ef0d19fa66e29d0411cfaac1f7
HaoFrank <HaoFrank@users.noreply.github.com>
2017-05-25 15:30:01 +0800

    使用helper重构代码

--- app/Http/Controllers/QuestionFollowController.php
+++ app/Http/Controllers/QuestionFollowController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use App\Repositories\QuestionRepository;
 
 class QuestionFollowController extends Controller
 {
-    public function __construct()
+    protected $question;
+
+    public function __construct(QuestionRepository $question)
     {
         $this->middleware('auth');
+        $this->question = $question;
     }
 
     public function follow($question)
@@ -17,5 +21,29 @@ class QuestionFollowController extends Controller
          Auth::user()->followThis($question);
 
          return back();
+    }
+
+    public function follower(Request $request)
+    {
+        $followed = user('api')->followed($request->get('question'));
+
+        if ($followed) {
+            return response()->json(['followed'=>true ]);
+        }
+        return response()->json(['followed'=>false]);
+
+    }
+    public function followThisQuestion(Request $request)
+    {
+        $question = $this->question->byId($request->get('question'));
+        $followed = user('api')->followThis($question->id);
+
+        if (count($followed['detached'])>0) {
+            $question->decrement('followers_count');
+            return response()->json(['followed'=> false ]);
+        }
+
+        $question->increment('followers_count');
+        return response()->json(['followed'=>true]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,37 +17,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('api')->get('/topics', function (Request $request) {
-    $topics = App\Topic::select(['id','name'])->where('name', 'like', '%'.$request->query('q').'%')->get();
-    return $topics;
-});
+Route::middleware('api')->get('/topics', 'TopicsController@index');
 
-Route::post('/question/follower', function(Request $request)
-{
-    $user = Auth::guard('api')->user();
-    $followed = $user->followed($request->get('question'));
-
-    if ($followed) {
-        return response()->json(['followed'=>true ]);
-    }
-    return response()->json(['followed'=>false]);
-
-})->middleware('auth:api');
-
-Route::post('/question/follow', function(Request $request)
-{
-    $user = Auth::guard('api')->user();
-    $question = \App\Question::find($request->get('question'));
-    $followed = $user->followThis($question->id);
-
-    if (count($followed['detached'])>0) {
-        $question->decrement('followers_count');
-        return response()->json(['followed'=> false ]);
-    }
-
-    $question->increment('followers_count');
-    return response()->json(['followed'=>true]);
-})->middleware('auth:api');
+Route::post('/question/follower', 'QuestionFollowController@follower')->middleware('auth:api');
+Route::post('/question/follow', 'QuestionFollowController@followThisQuestion')->middleware('auth:api');
 
 Route::get('/user/followers/{id}','FollowersController@index');
 Route::post('/user/follow', 'FollowersController@follow');
